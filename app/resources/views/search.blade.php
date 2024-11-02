a581aef7c1d42b2027c6bd86fca333c15e6330a2
vuviy <vova.banudz@gmail.com>
2024-11-02 17:14:36 +0200

    fix img

--- app/resources/views/search.blade.php
+++ app/resources/views/search.blade.php
@@ -101,7 +101,7 @@
 
             <div class="result-card d-flex" style="margin-bottom: 20px; width: 400px;">
                 <a href="{{route('userProfile', ['userId' => isset($people->user_id) ?: $people->id ])}}">
-                    <img  src="{{$people->gender != '2' ? 'https://th-thumbnailer.cdn-si-edu.com/5a79C6jJ8BrChMX5tgEKiMI_qqo=/1000x750/filters:no_upscale():focal(792x601:793x602)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/52/e4/52e44474-c2dc-41e0-bb77-42a904695196/this-image-shows-a-portrait-of-dragon-man-credit-chuang-zhao_web.jpg' : 'https://pbs.twimg.com/profile_images/1221735342937645067/MGMXudrc_400x400.jpg'}}" alt="Фото профілю" style="height: 200px; width: 200px">
+                    <img  src="{{$people->gender != 'male' ? 'https://th-thumbnailer.cdn-si-edu.com/5a79C6jJ8BrChMX5tgEKiMI_qqo=/1000x750/filters:no_upscale():focal(792x601:793x602)/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/52/e4/52e44474-c2dc-41e0-bb77-42a904695196/this-image-shows-a-portrait-of-dragon-man-credit-chuang-zhao_web.jpg' : 'https://pbs.twimg.com/profile_images/1221735342937645067/MGMXudrc_400x400.jpg'}}" alt="Фото профілю" style="height: 200px; width: 200px">
                 </a>
                     <div class="info" style="margin-left: 20px">
                         <a href="{{route('userProfile', $people)}}" style="text-decoration: none; color: black">
